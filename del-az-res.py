c44f84dd75a2ee08d330fb630154293ba8b03946
techslateramu <105220391+techslateramu@users.noreply.github.com>
2023-11-25 14:20:09 +0000

    fixed pipeline

--- del-az-res.py
+++ del-az-res.py
@@ -1,37 +1,29 @@
-import os
-from azure.identity import DefaultAzureCredential
+from azure.identity import ClientSecretCredential
 from azure.mgmt.resource import ResourceManagementClient
 
-# Replace these with your own values
-# subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID')
-subscription_id = "d9d24cf2-e4f1-4942-958f-a6bd839a3f1e"
+def delete_all_resources(subscription_id, client_id, client_secret, tenant_id):
+    # Create a ClientSecretCredential using explicit credentials
+    credential = ClientSecretCredential(
+        client_id=client_id,
+        client_secret=client_secret,
+        tenant_id=tenant_id
+    )
 
+    # Create a ResourceManagementClient using the credential
+    resource_client = ResourceManagementClient(credential, subscription_id)
 
-# Create a ResourceManagementClient
-resource_client = ResourceManagementClient(DefaultAzureCredential(), subscription_id)
+    # Get all resource groups in the subscription
+    resource_groups = resource_client.resource_groups.list()
 
+    for rg in resource_groups:
+        print(f"Deleting resource group: {rg.name}")
+        resource_client.resource_groups.begin_delete(rg.name).wait()
 
-# List all resource groups in the subscription
-resource_groups = resource_client.resource_groups.list()
+if __name__ == "__main__":
+    # Replace these values with your actual Azure subscription and application (service principal) details
+    subscription_id = "your-subscription-id"
+    client_id = "your-client-id"
+    client_secret = "your-client-secret"
+    tenant_id = "your-tenant-id"
 
-# Iterate through each resource group and delete resources
-for resource_group in resource_groups:
-    print(f"Deleting resources in resource group: {resource_group.name}")
-    
-    # List all resources in the resource group
-    resources = resource_client.resources.list_by_resource_group(resource_group.name)
-
-    # Delete each resource in the resource group
-    for resource in resources:
-        print(f"Deleting resource: {resource.name} - Type: {resource.type}")
-        
-        # Corrected code for deletion
-        resource_client.resources.begin_delete_by_id(resource.id, api_version='2023-04-01').wait()
-
-    print("Resource group cleared.")
-    
-    # Delete the resource group itself
-    resource_client.resource_groups.begin_delete(resource_group.name).wait()
-    print(f"Resource group {resource_group.name} deleted.")
-
-print("All resources and resource groups in the subscription deleted successfully.")
+    delete_all_resources(subscription_id, client_id, client_secret, tenant_id)
